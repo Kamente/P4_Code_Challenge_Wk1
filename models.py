feed7973d6c11abe6999ac76d5c2a32163d7bf19
Kamente <justinm48000@gmail.com>
2023-09-24 19:35:01 +0300

    models

---
+++ models.py
@@ -0,0 +1,28 @@
+from flask_slqalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+
+    pizzas = db.relationship('Pizza', backref='restaurants', lazy=True)
+
+
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    ingredients = db.Column(db.string(255), nullable=False)
+
+    restaurants = db.relationship('Restaurant', backref='pizzas', lazy=True)
+
+
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey(
+        "pizza.id"), nullable=False)  # foreign key for the pizza table
+    restaurant_id = db.Column(db.Integer, db.ForeignKey(
+        "restaurant.id"), nullable=False)  # foreign key for the restaurant table
